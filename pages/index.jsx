af67caf7cc61b01a0e987b3d7b92566b11e27cb3
Kazuma.I <51313744+Kazuma7@users.noreply.github.com>
2022-06-26 17:50:48 +0900

    chore: フロント完成

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,9 +2,13 @@ import Head from "next/head";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import { useMoralis, useNFTBalances, useMoralisWeb3Api } from "react-moralis";
-import CardList from "../components/CardList";
+import { ethers } from "ethers";
+import CollectionDetail from "../components/CollectionDetail";
+import collectionList from "../components/collectionList.json";
 
 export default function Home() {
+	const [isAstar, setIsAstar] = useState();
+	const [collectionNo, setCollectionNo] = useState(0);
 	const {
 		isAuthenticated,
 		user,
@@ -13,6 +17,22 @@ export default function Home() {
 		logout,
 		isLoggingOut,
 	} = useMoralis();
+
+	useEffect(() => {
+		const addChain = async () => {
+			try {
+				await window.ethereum.request({
+					method: "wallet_switchEthereumChain",
+					params: [{ chainId: "0x4" }],
+				});
+			} catch (Exeption) {
+				console.log("Rinkeby Network has already been connected.");
+			} finally {
+			}
+		};
+		addChain();
+	}, []);
+
 	if (!isAuthenticated) {
 		return (
 			<div className="h-screen w-screen bg-white">
@@ -48,24 +68,24 @@ export default function Home() {
 				</div>
 			</div>
 		);
-	}
-	return (
-		<div className="h-screen w-screen bg-white">
-			<div className="flex justify-between items-center px-10 h-[60px] border-b text-lg text-gray-700">
-				<div className="font-Inter-Medium">xBatter</div>
-				<a
-					className=" rounded-full bg-gray-100 hover:bg-gray-200 px-4 py-1 text-sm text-gray-500 cursor-pointer"
-					onClick={logout}
-				>
-					disconnect
-				</a>
-			</div>
-			<div className="flex justify-center items-center h-full w-full bg-slate-100">
-				<div>
-					<div></div>
-					<div></div>
+	} else {
+		return (
+			<div className="h-screen w-screen ">
+				<div className="flex justify-between items-center px-10 h-[60px] border-b text-lg text-gray-700">
+					<div className="font-Inter-Medium">xBatter</div>
+					<a
+						className=" rounded-full bg-gray-100 hover:bg-gray-200 px-4 py-1 text-sm text-gray-500 cursor-pointer"
+						onClick={logout}
+					>
+						disconnect
+					</a>
 				</div>
+				<CollectionDetail
+					collectionList={collectionList}
+					selCollectionNo={collectionNo}
+					changeState={setCollectionNo}
+				/>
 			</div>
-		</div>
-	);
+		);
+	}
 }
