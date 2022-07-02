1ed74b0df1c8ef68371c5f36d304ee0e4b32e043
Kazuma.I <51313744+Kazuma7@users.noreply.github.com>
2022-07-02 14:19:07 +0900

    add: コントラクト関数を設定

--- components/CardList.jsx
+++ components/CardList.jsx
@@ -1,21 +1,25 @@
-import Link from "next/link";
 import Card from "/components/Card.jsx";
 
-const CardList = ({ data }) => {
-  return (
-    <div className="bg-slate-100 grid grid-cols-2 gap-8 px-10 pb-10 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 max-w-screen-xl mx-auto mt-12">
-      {data &&
-        data.result.map((nft) => (
-          <Card
-            image={nft.image ? nft.image : "/images/noimage.png"}
-            c_name={nft.name ? nft.name : "unknown"}
-            name={nft.metadata ? nft.metadata.name : "unknown"}
-            price={0.01}
-            key=""
-          />
-        ))}
-    </div>
-  );
+const CardList = ({ data, menuFlg }) => {
+	return (
+		<div className="bg-slate-100 grid grid-cols-2 gap-8 px-10 pb-10 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 max-w-screen-xl mx-auto mt-12">
+			{data &&
+				data.map((nft) => {
+					return (
+						<Card
+							image={nft.image ? nft.image : "/images/noimage.png"}
+							c_name={nft.name ? nft.name : "unknown"}
+							name={nft.metadata ? nft.metadata.name : "unknown"}
+							price={0.01}
+							cntrAddr={nft.token_address ? nft.token_address : ""}
+							cntrTokenId={nft.token_id ? nft.token_id : ""}
+							key=""
+							menuFlg={menuFlg}
+						/>
+					);
+				})}
+		</div>
+	);
 };
 
 export default CardList;
--- components/CollectionDetail.jsx
+++ components/CollectionDetail.jsx
@@ -1,107 +1,154 @@
 import CardList from "../components/CardList";
 import Collection from "../components/Collection";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { ethers } from "ethers";
 
 const CollectionDetail = ({
-  changeState,
-  collectionList,
-  selCollectionNo,
-  ctrItemList,
-  yourItemList,
+	changeState,
+	collectionList,
+	selCollectionNo,
+	ctrItemList,
+	yourItemList,
 }) => {
-  const [menuIsOpen, setMenuIsOpen] = useState(true);
-  console.log(ctrItemList.result.length);
-  const stakeNftAmount = ctrItemList.result.length;
-  const yourTicketAmount = yourItemList.result.length;
+	const [menuIsOpen, setMenuIsOpen] = useState(true);
+	const [yourItemListFilter, setYourItemListFilter] = useState([]);
+	const [stakeAmount, setStakeAmount] = useState(0);
+	const [yourTicketAmount, setYourTicketAmount] = useState(0);
+	const [selCollectionItemList, setSelCollectionItemList] = useState();
+	const [yourTicketList, setYourTicketList] = useState();
 
-  const onChnageMenu = () => {
-    if (menuIsOpen == false) {
-      setMenuIsOpen(true);
-    } else if (menuIsOpen == true) {
-      setMenuIsOpen(false);
-    }
-  };
-  return (
-    <div className=" w-full bg-slate-100 h-full">
-      <div className="flex w-full bg-slate-100">
-        <div className="w-96 ">
-          <Collection
-            changeState={changeState}
-            collectionList={collectionList}
-            selCollectionNo={selCollectionNo}
-          />
-        </div>
-        {menuIsOpen ? (
-          <div className="w-full  ">
-            <div className="flex justify-between items-center mx-10 my-10 ">
-              <div className=" text-3xl font-bold">
-                {collectionList[selCollectionNo].cName}
-              </div>
-              <button
-                className=" px-4 rounded-md button-bg font-bold text-white py-2  shadow-md"
-                onClick={onChnageMenu}
-              >
-                Exchange Ticket
-              </button>
-            </div>
+	useEffect(() => {
+		//所有してるNFTから特定のNFTをフィルター
+		const tmpYourItemListFilter = yourItemList.result.filter(filterCollection);
+		setYourItemListFilter(tmpYourItemListFilter);
 
-            <div className="grid grid-cols-2 border bg-white mx-10 my-10 py-4 rounded-lg bg-opacity-70  shadow-md">
-              <div className="mx-auto">
-                <div className="text-center pb-2">Stake NFT Amount</div>
-                <div className="text-center text-3xl font-bold">
-                  {stakeNftAmount}
-                </div>
-              </div>
-              <div className="mx-auto">
-                <div className="text-center pb-2">Your Ticket Amount</div>
-                <div className="text-center text-3xl font-bold">
-                  {yourTicketAmount}
-                </div>
-              </div>
-            </div>
-            <CardList data={ctrItemList} />
+		//所有してるNFTから特定のNFTのチケットをフィルター
+		const tmpYourTicketList = yourItemList.result.filter(
+			filterCollectionTicket
+		);
+		setYourTicketList(tmpYourTicketList);
+		const tmpYourTicketAmount = tmpYourTicketList.length;
+		setYourTicketAmount(tmpYourTicketAmount);
+	}, [yourItemList]);
 
-            <div className="flex justify-between bg-slate-100 px-10 py-10">
-              <div>Creator</div>
-              <div className="flex justify-center">
-                <div>Github</div>
-                <div>discord</div>
-              </div>
-            </div>
-          </div>
-        ) : (
-          <div className="w-full ">
-            <div className="flex justify-between items-center mx-10 my-10">
-              <div className=" text-3xl font-bold">
-                Yours &quot;{collectionList[selCollectionNo].cName}&quot;
-              </div>
-              <button
-                className=" px-4 rounded-md bg-gray-400 font-bold text-white py-2  shadow-md"
-                onClick={onChnageMenu}
-              >
-                Exchange NFT
-              </button>
-            </div>
+	useEffect(() => {
+		//ステーキングしてあるNFTから特定のNFTをフィルター
+		const tmpSelCollectionItemList =
+			ctrItemList.result.filter(filterCollection);
+		setSelCollectionItemList(tmpSelCollectionItemList);
+		const tmpStakeAmount = tmpSelCollectionItemList.length;
+		setStakeAmount(tmpStakeAmount);
+	}, [ctrItemList]);
 
-            <div className=" border bg-white mx-10 my-10 py-4 rounded-lg bg-opacity-70  shadow-md">
-              <div className=" py-2 pl-6">
-                💡 Select your NFT you would like to exchange
-              </div>
-            </div>
-            <CardList data={yourItemList} />
+	const onChnageMenu = () => {
+		if (menuIsOpen == false) {
+			setMenuIsOpen(true);
+		} else if (menuIsOpen == true) {
+			setMenuIsOpen(false);
+		}
+	};
 
-            <div className="flex justify-between bg-slate-100 px-10 py-10">
-              <div>Creator</div>
-              <div className="flex justify-center">
-                <div>Github</div>
-                <div>discord</div>
-              </div>
-            </div>
-          </div>
-        )}
-      </div>
-    </div>
-  );
+	//特定のNFTフィルター
+	const filterCollection = (item) => {
+		if (
+			item.token_address.toUpperCase() ==
+			"0xea03e1b9022770b4c9b061be263d601f1fd1e22e".toUpperCase()
+		) {
+			return true;
+		}
+	};
+
+	//特定のNFTチケットフィルター
+	const filterCollectionTicket = (item) => {
+		if (
+			item.token_address.toUpperCase() ==
+			"0xd6B4239e23e3801c1adA8383830A59d27fdCb7Ec".toUpperCase()
+		) {
+			return true;
+		}
+	};
+
+	return (
+		<div className=" w-full bg-slate-100 h-full">
+			<div className="flex w-full bg-slate-100">
+				<div className="w-96 ">
+					<Collection
+						changeState={changeState}
+						collectionList={collectionList}
+						selCollectionNo={selCollectionNo}
+					/>
+				</div>
+				{menuIsOpen ? (
+					<div className="w-full  ">
+						<div className="flex justify-between items-center mx-10 my-10 ">
+							<div className=" text-3xl font-bold">
+								{collectionList[selCollectionNo].cName}
+							</div>
+							<button
+								className=" px-4 rounded-md button-bg font-bold text-white py-2  shadow-md"
+								onClick={onChnageMenu}
+							>
+								Exchange Ticket
+							</button>
+						</div>
+
+						<div className="grid grid-cols-2 border bg-white mx-10 my-10 py-4 rounded-lg bg-opacity-70  shadow-md">
+							<div className="mx-auto">
+								<div className="text-center pb-2">Stake NFT Amount</div>
+								<div className="text-center text-3xl font-bold">
+									{stakeAmount}
+								</div>
+							</div>
+							<div className="mx-auto">
+								<div className="text-center pb-2">Your Ticket Amount</div>
+								<div className="text-center text-3xl font-bold">
+									{yourTicketAmount}
+								</div>
+							</div>
+						</div>
+						<CardList data={ctrItemList.result} menuFlg={menuIsOpen} />
+
+						<div className="flex justify-between bg-slate-100 px-10 py-10">
+							<div>Creator</div>
+							<div className="flex justify-center">
+								<div>Github</div>
+								<div>discord</div>
+							</div>
+						</div>
+					</div>
+				) : (
+					<div className="w-full ">
+						<div className="flex justify-between items-center mx-10 my-10">
+							<div className=" text-3xl font-bold">
+								Yours &quot;{collectionList[selCollectionNo].cName}&quot;
+							</div>
+							<button
+								className=" px-4 rounded-md bg-gray-400 font-bold text-white py-2  shadow-md"
+								onClick={onChnageMenu}
+							>
+								Exchange NFT
+							</button>
+						</div>
+
+						<div className=" border bg-white mx-10 my-10 py-4 rounded-lg bg-opacity-70  shadow-md">
+							<div className=" py-2 pl-6">
+								💡 Select your NFT you would like to exchange
+							</div>
+						</div>
+						<CardList data={yourItemListFilter} menuFlg={menuIsOpen} />
+
+						<div className="flex justify-between bg-slate-100 px-10 py-10">
+							<div>Creator</div>
+							<div className="flex justify-center">
+								<div>Github</div>
+								<div>discord</div>
+							</div>
+						</div>
+					</div>
+				)}
+			</div>
+		</div>
+	);
 };
 
 export default CollectionDetail;
